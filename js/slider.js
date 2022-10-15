3a369896de23652a543c5f52cfe6b36627b85f6b
Roman <76131482+ronango@users.noreply.github.com>
2022-10-15 17:22:09 +0300

    Create slider.js

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".roadmap__swiper", {
+    slidesPerView: "auto",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+  });

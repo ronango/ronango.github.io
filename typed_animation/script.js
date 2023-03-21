44c047aabb08efecf22f3f62ce90594cc1da505b
ronango <romaneli.rs@gmail.com>
2023-03-21 21:13:48 +0300

    shab deploy

--- typed_animation/script.js
+++ typed_animation/script.js
@@ -3,7 +3,7 @@ const typed_strings = document.querySelector(".typed-items").innerHTML;
 var typed = new Typed('.typed', {
     strings: typed_strings.split("\n "),
     typeSpeed: 30,
-    backSpeed: 20,
+    backSpeed: 10,
     backDelay: 500,
     startDelay: 1000,
     loop: true,

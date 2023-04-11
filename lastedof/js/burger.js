935c4845f0d27c64ffed9c8fd8cee4f882c04c81
ronango <romaneli.rs@gmail.com>
2023-04-11 16:37:40 +0300

    new fix

---
+++ lastedof/js/burger.js
@@ -0,0 +1,17 @@
+let menuBtn = document.querySelector('.burger');
+let menu = document.querySelector('.burger-menu');
+let linkArray = document.querySelectorAll('.nav_link');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+    document.querySelector('body').classList.toggle('menu-active')
+})
+
+linkArray.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log('sdfsdf')
+        menu.classList.toggle('active');
+        menuBtn.classList.toggle('active');
+        document.querySelector('body').classList.toggle('menu-active')
+    })
+})
